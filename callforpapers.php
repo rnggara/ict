b10058d47113d8df69159fa61691640fe4c8799b
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-13 08:49:06 +0700

    Add files via upload

---
+++ callforpapers.php
@@ -0,0 +1,118 @@
+<header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>Call For Papers</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+           <!--  <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p> -->
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+<section class="" id="about">
+	<div>
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h1 class="section-heading text-center">CALL FOR PAPERS</h1>
+            <hr class="my-4 mb-5">
+
+            <p class="mb-4" align="justify">The 4th ICTTE 2018 invites researchers, academicians, &nbsp;educators, practitioners, government&nbsp; agencies, and consultants around the world to attend and share their insights, perspectives and new trends of researches which relate to the theme of the conference, </p>
+
+            <h3 class="mb-4" align="center"><strong>“STRENGTHENING THE NETWORK AND COLLABORATION TO ANTICIPATE INFORMATION AND EDUCATION DISRUPTION.”</strong></h3>
+            
+            <p class="mb-3" align="justify">Manuscript submitted to the 4th ICTTE 2018 should adhere to the manuscript submission guideline. The paper guideline is available at of <strong> the official website</strong>. The submission should be made to the 4th ICTTE 2018’s conference system in MS Word (.doc, .docx) format; then, each manuscript will be reviewed by independent reviewers. It is mandatory that the author of each accepted papers to present their works in the designated time slots of the conference for the article to be considered for publication in the <strong>ICTTE 2018’s publication outlets</strong>.</p>            
+            
+          </div>
+        </div>
+      </div>
+	</div>
+</section>
+
+<section class="bg-light" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="col-lg-12 text-center">
+            <h2 class="section-heading">AREA OF RESEARCH</h2>
+            <hr class="my-4 mb-5">
+        </div>
+        <div class="col-lg-8 mx-auto">
+          <table class="table table-striped bg-light">
+            <tr class="active">
+              <th>Institutional Quality of Teacher Training & Education</th>
+              <td class="text-muted">
+                <ul>
+                  <li>New Policy on Professional Development of Teacher</li>
+                  <li>Quality Assurance and Accreditation of Teacher Training and Education Institution</li>
+                  <li>Comparative on TTE policy and practices</li>
+                  <li>Student Board and Activities</li>
+                  <li>Trends in International Partnership (double degree, co-supervisor, etc)</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>              
+              <th>Teacher Training & Education Curricula</th>             
+              <td class="text-muted">
+                <ul>
+                  <li>National and Standard Curriculum of Teacher Training and Education</li>
+                  <li>International and Regional Curriculum of Teacher Training and Education</li>
+                  <li>Qualification of Teacher Candidate</li>
+                  <li>Learning Outcomes of Teacher Training and Education</li>
+                  <li>Gender Issue and Multicultural education</li>
+                </ul>
+              </td>
+            </tr>          
+            <tr class="active">              
+              <th>Innovation in Teaching, Learning, and Assessment of Teacher Training and Education</th>             
+              <td class="text-muted">
+                <ul>
+                  <li>Learning method and strategies</li>
+                  <li>Trends in technology of teaching &amp; learning</li>
+                  <li>Modern instruction</li>
+                  <li>Assessment</li>                  
+                </ul>
+              </td>
+            </tr>                 
+            <tr class="active">
+              <th>Course Development and Teaching Practices in Teacher Training & Education</th>
+              <td class="text-muted">
+                <ul>
+                  <li>Mathematics &amp; Science Education</li>
+                  <li>Social Science Education</li>
+                  <li>Language Education</li>
+                  <li>Vocational Education</li>
+                  <li>Early Childhood Education</li>
+                  <li>Primary Education</li>
+                  <li>Secondary Education</li>
+                </ul>
+              </td>
+            </tr>          
+            <tr>
+              <th>School Practice</th>
+              <td class="text-muted">
+                <ul>                
+                  <li>Teachers’ Learning Community</li>
+                  <li>School Management &amp; Reform</li>
+                  <li>Clinical experiences from the school counseling</li>
+                </ul>
+              </td>
+            </tr>
+            <tr>
+              <th>Educational Research Methodss</th>
+              <td class="text-muted">
+                <ul>
+                  <li>Classroom Action research</li>
+                  <li>Qualitative Research Methods</li>
+                  <li>Quantitative Research Application</li>                  
+                </ul>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </section>
