64759489a25637ee1f8b69eccbcfe647206654af
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-13 08:50:06 +0700

    Add files via upload

---
+++ template.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>ICTTE</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="<?php echo base_url(); ?>assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="<?php echo base_url(); ?>assets/vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url(); ?>assets/css/creative.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="<?php echo base_url(); ?>">ICTTE 2018</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>callforpapers">Call For Papers</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>publication">Publication</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>registration">Registration</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url(); ?>submission">Submission</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="">Sponsorship &amp; Exhibition</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="<?php echo base_url(); ?>#contact" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Info &amp; Contact</a>
+              <ul class="dropdown-menu nav-item bg-dark">
+                <li><a href="#" class="nav-link dropdown-item text-white">Item1</a></li>
+                <li><a href="#" class="nav-link dropdown-item text-white">Item1</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <?php echo $contents ?>
+
+    <footer>
+      <section class="bg-dark">
+        <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <h3 class="mb-3 section-heading text-white">Quick Links</h3>
+              <hr class="my-4">
+             <ul class="section-footer">
+               <li><a href="#" class="text-muted">Call For Papers</a></li>
+               <li><a href="#" class="text-muted">Publication</a></li>
+               <li><a href="#" class="text-muted">Registration</a></li>
+               <li><a href="#" class="text-muted">Submission</a></li>
+             </ul>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <h3 class="mb-3 section-heading text-white">The Conferences</h3>
+              <hr class="my-4">
+             <ul class="section-footer">
+               <li><a href="#" class="text-muted">ICTTE 2014</a></li>
+               <li><a href="#" class="text-muted">ICTTE 2015</a></li>
+               <li><a href="#" class="text-muted">ICTTE 2016</a></li>
+               <li><a href="#" class="text-muted">ICTTE 2017</a></li>
+             </ul>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <h3 class="mb-3 text-white">Secretariat</h3>
+              <hr class="my-4">
+               <p class="text-muted">Jl. Ahmad Yani No. 40, Solo, Kerten, Laweyan, Kota Surakarta, Jawa Tengah 57143 ictte@mail.uns.ac.id (62-271) 669124</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="<?php echo base_url(); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="<?php echo base_url(); ?>assets/js/creative.min.js"></script>
+
+  </body>
+
+</html>
