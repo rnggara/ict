53dc2a257ebeb935701a8c2cddb592801fb2a426
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-13 17:33:44 +0700

    Add files via upload

---
+++ application/views/comittee.php
@@ -0,0 +1,49 @@
+<header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <strong>COMITTEE</strong>
+            </h1>
+            <hr>
+          </div>
+          <!-- <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5">Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
+          </div> -->
+        </div>
+      </div>
+    </header>
+
+    <section class="" id="about">
+  <div>
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h1 class="section-heading text-center">ORGANISING COMMITTEE</h1>
+            <hr class="my-4 mb-5">
+
+            <ul class="mb-5 section-footer" align="center">
+              <li>Dr. Agus Dwi Anggono (Chair)</li>
+              <li>Dr. Mochamad Solikin (Co-Chair)</li>
+              <li>Ms. Taurista Perdana Syawitrim S.T., M.Sc (Secretary)</li>
+              <li>Ms. Ida Nursanti, S.T., M.Sc. (Treasurer)</li>
+              <li>Muchlison Anis, S.T., M.T. (Program Chair)</li>
+              <li>Ms. Dian Sari Maisaroh, S.Pi., M.Si. (Secretariat)</li>
+            </ul>
+
+           <!--  <div class="col-lg-8 mx-auto mb-5 text-center">
+             <a class="btn btn-primary btn-xl js-scroll-trigger" href="http://www.atlantis-press.com/index_atlantis_press.html?http%3A//www.atlantis-press.com/php/proceedings_ordering-2.php%3Fall" align="center">Atlantis Press</a>
+            </div>
+
+            <p class="mb-3" align="justify">Other papers will be published in International Journal of Pedagogy and Teacher Education (IJPTE).&nbsp;Excellent papers will be proposed to be submitted to Issues in Educational Research (Scopus Indexed).</p>
+
+            <div class="col-lg-8 mx-auto mb-5 text-center">
+             <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://jurnal.uns.ac.id/ijpte" align="center">IJPTE</a>
+            </div> -->
+            
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
