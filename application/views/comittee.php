482bd71b2ae6884c72e71050a0b89ce49b2b52b8
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-13 17:39:22 +0700

    Add files via upload

--- application/views/comittee.php
+++ application/views/comittee.php
@@ -21,6 +21,39 @@
         <div class="row">
           <div class="col-lg-8 mx-auto">
             <h1 class="section-heading text-center">ORGANISING COMMITTEE</h1>
+            <hr class="my-4 mb-5 primary">
+
+            <ul class="mb-5 section-footer" align="center">
+              <li>Dr. Agus Dwi Anggono (Chair)</li>
+              <li>Dr. Mochamad Solikin (Co-Chair)</li>
+              <li>Ms. Taurista Perdana Syawitrim S.T., M.Sc (Secretary)</li>
+              <li>Ms. Ida Nursanti, S.T., M.Sc. (Treasurer)</li>
+              <li>Muchlison Anis, S.T., M.T. (Program Chair)</li>
+              <li>Ms. Dian Sari Maisaroh, S.Pi., M.Si. (Secretariat)</li>
+            </ul>
+
+           <!--  <div class="col-lg-8 mx-auto mb-5 text-center">
+             <a class="btn btn-primary btn-xl js-scroll-trigger" href="http://www.atlantis-press.com/index_atlantis_press.html?http%3A//www.atlantis-press.com/php/proceedings_ordering-2.php%3Fall" align="center">Atlantis Press</a>
+            </div>
+
+            <p class="mb-3" align="justify">Other papers will be published in International Journal of Pedagogy and Teacher Education (IJPTE).&nbsp;Excellent papers will be proposed to be submitted to Issues in Educational Research (Scopus Indexed).</p>
+
+            <div class="col-lg-8 mx-auto mb-5 text-center">
+             <a class="btn btn-primary btn-xl js-scroll-trigger" href="https://jurnal.uns.ac.id/ijpte" align="center">IJPTE</a>
+            </div> -->
+            
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+
+<section class="bg-light" id="about">
+  <div>
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+            <h1 class="section-heading text-center">SCIENTIFIC COMMITTEE</h1>
             <hr class="my-4 mb-5">
 
             <ul class="mb-5 section-footer" align="center">
