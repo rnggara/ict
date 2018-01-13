7b63dae1ce0108bbcceab63bf94883f62a7d2c49
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-13 16:25:53 +0700

    Add files via upload

--- application/views/registration.php
+++ application/views/registration.php
@@ -15,92 +15,77 @@
       </div>
     </header>
 
-    <section class="bg-light" id="portfolio">
+    <section>
       <div class="container-fluid p-0">
         <div class="col-lg-12 text-center">
-            <h2 class="section-heading">Registration Fee</h2>
+            <h2 class="section-heading text-uppercase">Registration Fee</h2>
             <hr class="my-4 mb-5">
         </div>
         <div class="col-lg-8 mx-auto">
-          <table class="table table-striped bg-light" width="486">
+          <table class="table table-bordered" width="486">
+            <tr><td rowspan="3" class="table-dark" align="center">Participation Type</td><td class="table-dark" colspan="4" align="center" >Fees</td></tr>
+            
+            <tr><td colspan="2" align="center">Indonesian</td><td colspan="2" align="center">International</td></tr>
+
+            <tr class="bg-secondary"><td align="center">Early-Bird</td><td align="center">Regular</td>
+            <td align="center">Early-Bird</td><td align="center">Regular</td></tr>
 <tbody>
-<tr>
-<td style="text-align: center;" width="144"><strong>TYPE OF PARTICIPANTS</strong></td>
-<td style="text-align: center;" width="156"><strong>EARLY BIRD PAYMENT</strong></td>
-<td style="text-align: center;" width="186"><strong>REGULAR PAYMENT</strong></td>
-</tr>
-<tr>
-<td colspan="3" width="486">
-<p style="text-align: center;"><strong>PRESENTER</strong></p>
-</td>
-</tr>
-<tr>
-<td width="144"><strong>NON STUDENT</strong></td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
-<tr>
-<td width="144">INDONESIAN</td>
-<td width="156">IDR 1,500,000</td>
-<td width="186">IDR 1,700,000</td>
-</tr>
-<tr>
-<td width="144">NON INDONESIAN</td>
-<td width="156">USD 160</td>
-<td width="186">USD 170</td>
-</tr>
-<tr>
-<td width="144"><strong>STUDENT</strong></td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
-<tr>
-<td width="144">INDONESIAN</td>
-<td width="156">IDR 1.000,000</td>
-<td width="186">IDR &nbsp;1,200,000</td>
-</tr>
-<tr>
-<td width="144">NON INDONESIAN</td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
-<tr>
-<td colspan="3" width="486">
-<p style="text-align: center;"><strong>NON </strong><strong>PRESENTER</strong></p>
-</td>
-</tr>
-<tr>
-<td width="144"><strong>NON STUDENT</strong></td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
-<tr>
-<td width="144">INDONESIAN</td>
-<td width="156">IDR 1,000,000</td>
-<td width="186">IDR 1,200,000</td>
-</tr>
-<tr>
-<td width="144">NON INDONESIAN</td>
-<td width="156">USD 120</td>
-<td width="186">USD 130</td>
-</tr>
-<tr>
-<td width="144"><strong>STUDENT</strong></td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
-<tr>
-<td width="144">INDONESIAN</td>
-<td width="156">IDR 850,000</td>
-<td width="186">IDR 1,000,000</td>
-</tr>
-<tr>
-<td width="144">NON INDONESIAN</td>
-<td width="156"></td>
-<td width="186"></td>
-</tr>
+<tr><td colspan="5">Presenter</td></tr>
+<tr class="bg-secondary"><td>General</td><td class="text-white">IDR 2.500.000</td><td class="text-white">IDR 2.800.000</td><td class="text-white">USD 200</td><td class="text-white">USD 225</td></tr>
+
+<tr class="bg-secondary"><td>Student</td><td class="text-white">IDR 1.800.000</td><td class="text-white">IDR 2.000.000</td><td class="text-white">USD 150</td><td class="text-white">USD 175</td></tr>
+
+<tr><td>General Participant</td><td class="text-muted" colspan="2" align="center">IDR 1.000.000</td><td class="text-muted" colspan="2" align="center">USD 100</td></tr>
+
+<tr><td>Additional Paper(s)</td><td class="text-muted" colspan="2" align="center">IDR 1.000.000</td><td class="text-muted" colspan="2" align="center">USD 100</td></tr>
 </tbody>
 </table>
+
+
         </div>
       </div>
     </section>
+
+<section class="bg-light">
+  <div>
+    <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto">
+
+            <p class="mb-3">A huge discount will be given for the second paper of the same first author.</p>
+
+            <p class="mb-5">The fees include the cost for publication of the selected papers in the Scopus indexed journal. </p>
+
+            <p class="mb-5 text-muted"><strong>Cancellations: </strong>In case of cancellation for any reason, the conference registration fees will not be refunded, but substitutes will be accepted at any time provided that the conference committee is advised of the change.</p>
+
+            <p class="mb-3">Payment should be made payable to</p>
+
+            <table class="mb-5">
+              <tr>
+                <th>Account Name </th>
+                <td>: RPL 028 UNS UTK OPERASIONAL BLU 3</td>
+              </tr>
+              <tr>
+                <th>Bank Name</th>
+                <td>: BNI 46</td>
+              </tr>
+              <tr>
+                <th>Account No.</th>
+                <td>: 0034104488</td>
+              </tr>
+              <tr>
+                <th>Swift Code</th>
+                <td>:  BNINIDJAXXX</td>
+              </tr>
+            </table>
+
+            <p class="mb-3">Payment in IDR please <strong>add “18”</strong> in the amount of payment. For example: IDR 2,500,018</p>
+
+            <p class="mb-3">Please email the scanned of payment receipt to <a href="mailto:ictte@fkip.uns.ac.id">ictte@fkip.uns.ac.id</a> to confirm your payment. For quicker response, please put your email subject under <strong>Payment_Paper ID_Name </strong> format.</p>
+            
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+
