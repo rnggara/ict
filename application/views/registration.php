adbbc5dd6fbbe396ff301ec5a9ba4f95c1d58137
chaalyaa <34159939+chaalyaa@users.noreply.github.com>
2018-01-19 13:40:27 +0700

    Add files via upload

--- application/views/registration.php
+++ application/views/registration.php
@@ -1,75 +1,143 @@
-
     <section id="about">
+
       <div class="container-fluid p-0">
+
         <div class="col-lg-12 text-center">
+
             <h2 class="section-heading text-uppercase">Registration Fee</h2>
+
             <hr class="my-4 mb-5">
+
         </div>
+
         <div class="col-lg-10 mx-auto">
-          <table class="table table-bordered" width="486">
+
+          <table class="table table-bordered col-lg-10 col-sm-12">
+
             <tr><td rowspan="3" class="table-dark" align="center">Participation Type</td><td class="table-dark" colspan="4" align="center" >Fees</td></tr>
+
             
+
             <tr><td colspan="2" align="center">Indonesian</td><td colspan="2" align="center">International</td></tr>
 
+
+
             <tr class="bg-secondary"><td align="center">Early-Bird</td><td align="center">Regular</td>
+
             <td align="center">Early-Bird</td><td align="center">Regular</td></tr>
+
 <tbody>
+
 <tr><td colspan="5">Presenter</td></tr>
+
 <tr class="bg-secondary"><td>General</td><td class="text-white">IDR 2.500.000</td><td class="text-white">IDR 2.800.000</td><td class="text-white">USD 200</td><td class="text-white">USD 225</td></tr>
+
+
 
 <tr class="bg-secondary"><td>Student</td><td class="text-white">IDR 1.800.000</td><td class="text-white">IDR 2.000.000</td><td class="text-white">USD 150</td><td class="text-white">USD 175</td></tr>
 
+
+
 <tr><td>General Participant</td><td class="text-muted" colspan="2" align="center">IDR 1.000.000</td><td class="text-muted" colspan="2" align="center">USD 100</td></tr>
 
+
+
 <tr><td>Additional Paper(s)</td><td class="text-muted" colspan="2" align="center">IDR 1.000.000</td><td class="text-muted" colspan="2" align="center">USD 100</td></tr>
+
 </tbody>
+
 </table>
 
 
+
+
+
         </div>
+
       </div>
+
     </section>
 
+
+
 <section class="bg-light">
+
   <div>
+
     <div class="container">
+
         <div class="row">
+
           <div class="col-lg-12 mx-auto">
+
+
 
             <p class="mb-3">A huge discount will be given for the second paper of the same first author.</p>
 
+
+
             <p class="mb-5">The fees include the cost for publication of the selected papers in the Scopus indexed journal. </p>
+
+
 
             <p class="mb-5 text-muted"><strong>Cancellations: </strong>In case of cancellation for any reason, the conference registration fees will not be refunded, but substitutes will be accepted at any time provided that the conference committee is advised of the change.</p>
 
+
+
             <p class="mb-3">Payment should be made payable to</p>
 
+
+
             <table class="mb-5">
+
               <tr>
+
                 <th>Account Name </th>
+
                 <td>: RPL 028 UNS UTK OPERASIONAL BLU 3</td>
+
               </tr>
+
               <tr>
+
                 <th>Bank Name</th>
+
                 <td>: BNI 46</td>
+
               </tr>
+
               <tr>
+
                 <th>Account No.</th>
+
                 <td>: 0034104488</td>
+
               </tr>
+
               <tr>
+
                 <th>Swift Code</th>
+
                 <td>:  BNINIDJAXXX</td>
+
               </tr>
+
             </table>
 
-            <p class="mb-3">Payment in IDR please <strong>add “18”</strong> in the amount of payment. For example: IDR 2,500,018</p>
 
-            <p class="mb-3">Please email the scanned of payment receipt to <a href="mailto:ictte@fkip.uns.ac.id">ictte@fkip.uns.ac.id</a> to confirm your payment. For quicker response, please put your email subject under <strong><i>Payment_Paper ID_Name</i></strong> format.</p>
+
+            <p class="mb-3">Payment in IDR please <strong>add “18”</strong> in the amount of payment. For example: IDR 2,500,018</p>
             
+
           </div>
+
         </div>
+
       </div>
+
   </div>
+
 </section>
+
+
 
